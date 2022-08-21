39d5c4f893aee4d0c36621385588befe00af2b1e
zlx01 <761451281@qq.com>
2022-08-21 12:16:53 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+
+function handleMove(e) {
+  // 计算鼠标位置相对于起始刻度的距离
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + '%';
+  const playbackRate = percent * (max - min) + min;
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + '×';
+  video.playbackRate = playbackRate;
+}
+
+speed.addEventListener('mousemove', handleMove);
